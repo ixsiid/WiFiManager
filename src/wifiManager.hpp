e7a10aaa83bc57c8e0ef10c2e753e57330c3089b
Tal <35385895+ixsiid@users.noreply.github.com>
2021-12-31 18:32:58 +0900

    Update wifiManager.hpp

--- src/wifiManager.hpp
+++ src/wifiManager.hpp
@@ -2,20 +2,21 @@
 #define __UTILITY_WIFI_H
 
 #include <lwip/inet.h>
+#include <esp_netif_ip_addr.h>
 
 class WiFi {
     private:
 	WiFi();
 	static bool initialized;
-	static ip4_addr_t ip;
-	static ip4_addr_t gateway;
-	static ip4_addr_t subnetmask;
+	static esp_ip4_addr_t ip;
+	static esp_ip4_addr_t gateway;
+	static esp_ip4_addr_t subnetmask;
 	static bool connected;
 
     public:
 	static bool Connect(const char* ssid, const char* password);
 	static bool Disconnect(bool release = false);
-	static ip4_addr_t *getIp();
+	static esp_ip4_addr_t *getIp();
 };
 
 #endif // __UTILITY_WIFI_H
